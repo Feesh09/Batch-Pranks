fb18b64bf643b0e62de728fce2abf39bdc2a5b72
Sergeant Fuzzyboots <91094449+Feesh09@users.noreply.github.com>
2022-11-03 11:38:20 -0500

    Add files via upload

---
+++ license.bat
@@ -0,0 +1,55 @@
+@ echo off
+echo                   Property of Idiot Games
+echo                          __    _                                   
+echo                     _wr**        *-q__                             
+echo                  _dP                 9m_     
+echo                _#P                     9#_                         
+echo               d#@                       9#m                        
+echo              d##                         ###                       
+echo             J###                         ###L                      
+echo             {###K                       J###K                      
+echo             ]####K      ___aaa___      J####F                      
+echo         __gmM######_  w#P**   **9#m  _d#####Mmw__                  
+echo      _g##############mZ_         __g##############m_               
+echo    _d####M@PPPP@@M#######Mmp gm#########@@PPP9@M####m_             
+echo   a###**          ,Z*#####@* '######'\g          **M##m            
+echo  J#@*             0L  **##     ##@'  J#              *#K           
+echo  #*               `#    *_gmwgm_~    dF               `#_          
+echo                    '#_   ]#####F   _dK                 JE          
+echo                      *m__ ##### __g@'                   F          
+echo                        'PJ#####LP'                                 
+echo  `                       0######_                      '           
+echo                        _0########_                                   
+echo      .               _d#####$#####m__              ,              
+echo       '*w_________am#####P'   ~9#####mw_________w*'                  
+echo           ''9@#####@M''           ''P@#####@M''  
+pause
+echo DEFINITION (section 1)
+echo contents of [Idiot Game](https://github.com/Feesh09/harmless-batch-viruses) are not to be: redistrubuted "275" (section 2), modified "385" (section 3), used for personal gain "184" (section 4), or unlawful intent "473" (section 5).
+echo in the event of any lawful action taken against user is by definition not our problem.
+echo SECTION 2
+echo Files are not to be redistrubuted.
+echo If files are discovered on any platfrom unless published by original distributer.
+echo All copies will be requestged to be taken down after 2 warnings the account will be reported for fruadulint activity.
+echo SECTION 3
+echo All files are not recomended to be modified.
+echo all modified files are no longer Idiot Games resposability as it is no longer there product.
+echo SECTION 4
+echo Personal gain off of Idiot Games property is forbidden.
+echo As Idiot Games does not profit from these files neither should you.
+echo SECTION 5
+echo Any unlawful action taken with Idiot Games files is not our resposability.
+echo In the event of lawful action taken against the files user, Idiot Games is not responsable.
+echo We post for education only not harm to anyone or anything.
+echo SECTION 6
+echo all users/companies listed below is to be excluded to anything marked "###".
+echo 184 = personal gain exclusions
+echo 275 = redistribution exclusions
+echo 385 = modification exclusions
+echo 473 = unlawful intent exclusions
+echo "184" = [no exclusions at the time of %date%]
+echo "275" = [no exclusions at the time of %date%]
+echo "385" = [no exclusions at the time of %date%]
+echo "473" = [no exclusions at the time of %date%]
+
+echo (last edit date = Nov 3, 2022)
---
+++ tableofcontents_credits.bat
@@ -0,0 +1,36 @@
+@echo off
+echo Table of Contents
+pause
+echo Feesh09/v0.0.1/license.bat
+echo 184 = personal gain exclusions
+echo 275 = redistribution exclusions
+echo 385 = modification exclusions
+echo 473 = unlawful intent exclusions
+pause
+echo Feesh09/v0.0.1/files/30minshutdownweek.bat
+echo line 2-3: initial warning
+echo line 5-53: shutdown times
+pause
+echo Feesh09/v0.0.1/files/cmdflood.bat
+echo line 2-4: initial warning
+echo line 10-26: random info, spam open loop, shutdown
+pause
+echo Feesh09/v0.0.1/files/justwhy.bat
+echo line 2-3: initial warning
+echo line 4-26: insults, disable mouse
+echo line 27-44: timout, spam open if close attempt
+pause
+echo Feesh09/v0.0.1/files/nope.bat
+echo line 2-3: initial warning
+echo line 4: shutdown
+pause
+echo Feesh09/v0.0.1/files/SkyNet.bat
+echo line 2-3: initial warning
+echo line 5-27: fake robot boot up with styling and functions
+pause
+echo Writers / Programmers
+echo 1. William James Latham (Feesh09)
+echo 2. Github
+echo 3. Microsoft Notepad
+pause
+echo I hope you enjoyed all of these pranks. Be smart with how you use the :)
