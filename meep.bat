099be7b50209067573eb87a8bba7337e3a3e4fe2
Sergeant Fuzzyboots <91094449+Feesh09@users.noreply.github.com>
2022-11-04 12:59:03 -0500

    Update meep.bat

--- meep.bat
+++ meep.bat
@@ -1,4 +1,7 @@
 @echo off
+echo x=msgbox("All files here are to be used for educational purposes only. Use all files as is; any modifications in any way immediatly deem them as no longer our responsibility. Any lawful action taken against individuals or an organization from use/distribution of these files is not our responsibility regardless of files edits or lack thereof (after posted publish date)." ,16, "WARNING") >> msgbox.vbs
+start msgbox.vbs
+pause
 set loopcount=[10000]
 :loop
 echo meep
