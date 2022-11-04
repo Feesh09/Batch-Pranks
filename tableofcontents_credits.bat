43d8d2e2ba31998827ede7eb1cf2ef122acf6dd1
Sergeant Fuzzyboots <91094449+Feesh09@users.noreply.github.com>
2022-11-04 11:37:45 -0500

    Update tableofcontents_credits.bat

--- tableofcontents_credits.bat
+++ tableofcontents_credits.bat
@@ -1,30 +1,30 @@
 @echo off
 echo Table of Contents
 pause
-echo Feesh09/v0.0.1/license.bat
+echo Feesh09/Batch-Pranks/license.bat
 echo 184 = personal gain exclusions
 echo 275 = redistribution exclusions
 echo 385 = modification exclusions
 echo 473 = unlawful intent exclusions
 pause
-echo Feesh09/v0.0.1/files/30minshutdownweek.bat
+echo Feesh09/Batch-Pranks/files/30minshutdownweek.bat
 echo line 2-3: initial warning
 echo line 5-53: shutdown times
 pause
-echo Feesh09/v0.0.1/files/cmdflood.bat
+echo Feesh09/Batch-Pranks/files/cmdflood.bat
 echo line 2-4: initial warning
 echo line 10-26: random info, spam open loop, shutdown
 pause
-echo Feesh09/v0.0.1/files/justwhy.bat
+echo Feesh09/Batch-Pranks/files/justwhy.bat
 echo line 2-3: initial warning
 echo line 4-26: insults, disable mouse
 echo line 27-44: timout, spam open if close attempt
 pause
-echo Feesh09/v0.0.1/files/nope.bat
+echo Feesh09/Batch-Pranks/files/nope.bat
 echo line 2-3: initial warning
 echo line 4: shutdown
 pause
-echo Feesh09/v0.0.1/files/SkyNet.bat
+echo Feesh09/Batch-Pranks/files/SkyNet.bat
 echo line 2-3: initial warning
 echo line 5-27: fake robot boot up with styling and functions
 pause
